d5a5b30ec9f0b78aec11e616d9fc55a7c3838f67
sanjeewa.senevirathna <sancyclops@gmail.com>
2022-12-31 13:16:47 +0530

    add email

--- webapp/student_notification.php
+++ webapp/student_notification.php
@@ -12,41 +12,26 @@ require 'phpmailer/src/SMTP.php';
 //Code for deletion
 if (isset($_GET['editid'])) {
 
-    $mail = new PHPMailer(true);                    // Passing `true` enables exceptions
-    try {
-        //Server settings
-        $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-        $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = 'smtp.gmail.com';                  // Specify main and backup SMTP servers
-        $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'ekanyakekanchanamala@gmail.com';             // SMTP username
-        $mail->Password = 'kanchana23';                           // SMTP password
-        $mail->SMTPSecure = 'ssl';                            // Enable SSL encryption, TLS also accepted with port 465
-        $mail->Port = 465;                                    // TCP port to connect to
+    $mail = new PHPMailer(); // create a new object
+    $mail->IsSMTP(); // enable SMTP
+    $mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
+    $mail->SMTPAuth = true; // authentication enabled
+    $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 465; // or 587
+    $mail->IsHTML(true);
+    $mail->Username = "ekanayakekanchanamala@gmail.com";
+    $mail->Password = "rmqqcihbwzpqrham";
+    $mail->SetFrom("ekanayakekanchanamala@gmail.com");
+    $mail->Subject = "Test";
+    $mail->Body = "hello";
+    $mail->AddAddress("sancyclops@gmail.com");
 
-        //Recipients
-        $mail->setFrom('ekanyakekanchanamala@gmail.com', 'Mailer');          //This is the email your form sends From
-        $mail->addAddress('ekanyakekanchanamala@gmail.com', 'Joe User'); // Add a recipient address
-        //$mail->addAddress('contact@example.com');               // Name is optional
-        //$mail->addReplyTo('info@example.com', 'Information');
-        //$mail->addCC('cc@example.com');
-        //$mail->addBCC('bcc@example.com');
-
-        //Attachments
-        //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
-        //Content
-        $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = 'Subject line goes here';
-        $mail->Body    = 'Body text goes here';
-        //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-        $mail->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo 'Message could not be sent.';
-        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    if(!$mail->Send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    } else {
+        #echo "Message has been sent";
+        echo "<script>alert('Notification has been sent');</script>";
     }
 
 }
