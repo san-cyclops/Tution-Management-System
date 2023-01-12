6c1d843231b8614200ebc4b99cfc539cdc76756e
sanjeewa.senevirathna <sancyclops@gmail.com>
2023-01-13 03:02:57 +0530

    add email

--- webapp/attendance.php
+++ webapp/attendance.php
@@ -3,58 +3,101 @@ session_start();
 //Database Connection
 include('dbconnection.php');
 require_once('function.php');
-$rfidno ="";
-$StudentID ="";
-$NIC ="";
-$StudentName ="";
+$rfidno = "";
+$StudentID = "";
+$NIC = "";
+$StudentName = "";
 if (isset($_POST['submit'])) {
 
     $rid = $_POST['rfid'];
+    if ($rid != "") {
+
+        $sql = mysqli_query($con, "select * from tblstudents where RFIDNumber=$rid");
+        $row = mysqli_num_rows($sql);
+        if ($row > 0) {
+            while ($row = mysqli_fetch_array($sql)) :
+                $rfidno = $row['RFIDNumber'];
+                $StudentID = $row['StudentID'];
+                $NIC = $row['NIC'];
+                $StudentName = $row['StudentName'];
 
 
-    $sql = mysqli_query($con, "select * from tblstudents where RFIDNumber=$rid");
-    $row = mysqli_num_rows($sql);
-    if ($row > 0) {
-        while ($row = mysqli_fetch_array($sql)) :
-            $rfidno = $row['RFIDNumber'];
-            $StudentID = $row['StudentID'];
-            $NIC = $row['NIC'];
-            $StudentName = $row['StudentName'];
+                echo "<script>alert('Data Available');</script>";
 
-
-
-
-            echo "<script>alert('Data Available');</script>";
-
-        endwhile;
-    }
-    else{
+            endwhile;
+        } else {
+            echo "<script>alert('Data UnAvailable');</script>";
+        }
+    } else {
         echo "<script>alert('Data UnAvailable');</script>";
     }
 
 
+}
+
+
+if (isset($_GET['delid'])) {
+
+    $rid = $_GET['delid'];
+    if ($rid != "") {
+        echo "<script>alert('Data deleted' $rid );</script>";
+        $strfidno ="";
+        $stStudentID="";
+        $stcourseid="";
+        $sql = mysqli_query($con, "SELECT a.id,a.RFIDNumber,b.StudentID,c.courseName,b.EmailAddress,a.courseId from tblstdenroll a 
+inner join tblstudents b on a.RFIDNumber=b.RFIDNumber inner join tblcourse c on a.courseId=c.id  where a.RFIDNumber=$rid");
+        $row = mysqli_num_rows($sql);
+        if ($row > 0) {
+            while ($row = mysqli_fetch_array($sql)) :
+                $strfidno = $row['RFIDNumber'];
+                $stStudentID = $row['id'];
+                $stcourseid = $row['courseId'];
+                echo "<script>alert('Data Available');</script>";
+
+            endwhile;
+
+            $attdate = date("Y-m-d");
+            $courseid = $stcourseid;
+            $studentid = $stStudentID;
+            $studentrfid = $rid;
+
+            echo "$courseid - $studentid -  $studentrfid -";
+
+
+            $sql = "INSERT INTO tblattendance(attdate,courseid,studentid,studentrfid) VALUES(?,?,?,?)";
+            $statement = $con->prepare($sql);
+            $statement->bind_param('ssss', $attdate, $courseid, $studentid, $studentrfid);
+
+            $current_id = $statement->execute() or die("<b>Error:</b> Problem on Insert<br/>" . mysqli_connect_error());
+
+            if ($current_id) {
+                echo "<script>alert('You have successfully inserted the data');</script>";
+                echo "<script type='text/javascript'> document.location ='attendance.php'; </script>";
+            } else {
+                echo "<script>alert('Something Went Wrong. Please try again');</script>";
+            }
+
+        }
+    }
 
 }
 ?>
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <!-- css style sheet -->
-  <link rel="stylesheet" href="css/pUser.css" />
-  <link rel="stylesheet" href="css/attendnce.css" />
-  <!-- font awesome file link -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" />
-  <!-- Boxicons CSS -->
-  <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
-  <title>Attendance</title>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!-- css style sheet -->
+    <link rel="stylesheet" href="css/pUser.css"/>
+    <link rel="stylesheet" href="css/attendnce.css"/>
+    <!-- font awesome file link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css"/>
+    <!-- Boxicons CSS -->
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet"/>
+    <title>Attendance</title>
 </head>
 <style>
     .button {
@@ -70,204 +113,223 @@ if (isset($_POST['submit'])) {
         cursor: pointer;
     }
 
-    .button2 {background-color: #008CBA;} /* Blue */
-    .button3 {background-color: #f44336;} /* Red */
-    .button4 {background-color: #e7e7e7; color: black;} /* Gray */
-    .button5 {background-color: #555555;} /* Black */
+    .button2 {
+        background-color: #008CBA;
+    }
+
+    /* Blue */
+    .button3 {
+        background-color: #f44336;
+    }
+
+    /* Red */
+    .button4 {
+        background-color: #e7e7e7;
+        color: black;
+    }
+
+    /* Gray */
+    .button5 {
+        background-color: #555555;
+    }
+
+    /* Black */
 </style>
 <body>
-  <div class="sidebar close">
+<div class="sidebar close">
     <div class="logo-details">
-      <a href="userP-Home.html" class="logo"><img src="img/logo.png" alt="logo" /></a>
-      <span class="logo_name">LeArN</span>
+        <a href="userP-Home.html" class="logo"><img src="img/logo.png" alt="logo"/></a>
+        <span class="logo_name">LeArN</span>
     </div>
     <ul class="nav-links">
-      <li>
-        <a href="">
-          <i class="bx bxs-grid"></i>
-          <span class="link_name">Dashboard</span>
-        </a>
-        <ul class="sub-menu blank">
-          <li><a href="" class="link_name">Dashboard</a></li>
-        </ul>
-      </li>
-      <li>
-        <div class="icon-link">
-          <a href="">
-            <i class="bx bxs-user-detail"></i>
-            <span class="link_name">Student</span>
-          </a>
-          <i class="bx bxs-chevron-down arrow"></i>
-        </div>
-        <ul class="sub-menu">
-          <li class="link-name"><a href="#">Student</a></li>
-          <li><a href="student_reg.html">Student Regitration</a></li>
-          <li><a href="studentEnrlmnt.php">Student Enrollment</a></li>
-          <li><a href="studentUpDlt.html">Student Update/Delete</a></li>
-        </ul>
-      </li>
-      <li>
-        <div class="icon-link">
-          <a href="">
-            <i class="bx bx-laptop"></i>
-            <span class="link_name">Course</span>
-          </a>
-          <i class="bx bxs-chevron-down arrow"></i>
-        </div>
-        <ul class="sub-menu">
-          <li class="link-name"><a href="#">Course</a></li>
-          <li><a href="addCourse.html">Add Course</a></li>
-          <li><a href="upDltCourse.html">Update/Delete Course</a></li>
-        </ul>
-      </li>
-      <li>
-        <div class="icon-link">
-          <a href="class.html">
-            <i class="bx bxs-school"></i>
-            <span class="link_name">Class</span>
-          </a>
+        <li>
+            <a href="">
+                <i class="bx bxs-grid"></i>
+                <span class="link_name">Dashboard</span>
+            </a>
+            <ul class="sub-menu blank">
+                <li><a href="" class="link_name">Dashboard</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="icon-link">
+                <a href="">
+                    <i class="bx bxs-user-detail"></i>
+                    <span class="link_name">Student</span>
+                </a>
+                <i class="bx bxs-chevron-down arrow"></i>
+            </div>
+            <ul class="sub-menu">
+                <li class="link-name"><a href="#">Student</a></li>
+                <li><a href="student_reg.html">Student Regitration</a></li>
+                <li><a href="studentEnrlmnt.php">Student Enrollment</a></li>
+                <li><a href="studentUpDlt.html">Student Update/Delete</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="icon-link">
+                <a href="">
+                    <i class="bx bx-laptop"></i>
+                    <span class="link_name">Course</span>
+                </a>
+                <i class="bx bxs-chevron-down arrow"></i>
+            </div>
+            <ul class="sub-menu">
+                <li class="link-name"><a href="#">Course</a></li>
+                <li><a href="addCourse.html">Add Course</a></li>
+                <li><a href="upDltCourse.html">Update/Delete Course</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="icon-link">
+                <a href="class.html">
+                    <i class="bx bxs-school"></i>
+                    <span class="link_name">Class</span>
+                </a>
 
-        </div>
-        <ul class="sub-menu blank">
-          <li class="link-name"><a href="class.html">Class</a></li>
-        </ul>
-      </li>
-      <li>
-        <div class="icon-link">
-          <a href="">
-            <i class="bx bx-bell"></i>
-            <span class="link_name">Notification</span>
-          </a>
-          <i class="bx bxs-chevron-down arrow"></i>
-        </div>
-        <ul class="sub-menu">
-          <li class="link-name"><a href="notification.php">Notification</a></li>
-          <li><a href="notification.php">Add Notification</a></li>
-          <li><a href="upNotification.html">Update Notification</a></li>
-        </ul>
-      </li>
-      <li>
-        <div class="icon-link">
-          <a href="">
-            <i class="bx bx-bell"></i>
-            <span class="link_name">Tutor</span>
-          </a>
-          <i class="bx bxs-chevron-down arrow"></i>
-        </div>
-        <ul class="sub-menu">
-          <li class="link-name"><a href="#">Tutor</a></li>
-          <li><a href="lectureReg.html">Add Tutor</a></li>
-          <li><a href="lectureUpDlt.html">Update/ Delete Tutor</a></li>
-        </ul>
-      </li>
-      <li>
-        <div class="icon-link">
-          <a href="">
-            <i class="bx bx-file-blank"></i>
-            <span class="link_name">Files</span>
-          </a>
-          <i class="bx bxs-chevron-down arrow"></i>
-        </div>
-        <ul class="sub-menu">
-          <li class="link-name"><a href="#">Files</a></li>
-          <li><a href="addFiles.html">Upload Files</a></li>
-          <li><a href="upDltFiles.html">Update/Delete Files </a></li>
-        </ul>
-      </li>
-      <li>
-        <div class="icon-link">
-          <a href="">
-            <i class="bx bxs-user"></i>
-            <span class="link_name">Staff</span>
-          </a>
-          <i class="bx bxs-chevron-down arrow"></i>
-        </div>
-        <ul class="sub-menu">
-          <li class="link-name"><a href="#">Staff Regitration</a></li>
-          <li><a href="staffReg.html">Staff Regitration</a></li>
-          <li><a href="staffUpDlt.html">Update/Delete Staff</a></li>
-        </ul>
-      </li>
-      <li>
-        <div class="icon-link">
-          <a href="attendance.html">
-            <i class="bx bx-calendar-check"></i>
-            <span class="link_name">Attendance</span>
-          </a>
-        </div>
-        <ul class="sub-menu blank">
-          <li class="link-name "><a href="attendance.html">Attendance</a></li>
-        </ul>
-      </li>
-      <li>
-        <div class="icon-link">
-          <a href="Payment.html">
-            <i class="bx bx-money"></i>
-            <span class="link_name">Payment</span>
-          </a>
-        </div>
-        <ul class="sub-menu blank">
-          <li><a href="Payment.html" class="link_name">Payment</a></li>
-        </ul>
-      </li>
+            </div>
+            <ul class="sub-menu blank">
+                <li class="link-name"><a href="class.html">Class</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="icon-link">
+                <a href="">
+                    <i class="bx bx-bell"></i>
+                    <span class="link_name">Notification</span>
+                </a>
+                <i class="bx bxs-chevron-down arrow"></i>
+            </div>
+            <ul class="sub-menu">
+                <li class="link-name"><a href="notification.php">Notification</a></li>
+                <li><a href="notification.php">Add Notification</a></li>
+                <li><a href="upNotification.html">Update Notification</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="icon-link">
+                <a href="">
+                    <i class="bx bx-bell"></i>
+                    <span class="link_name">Tutor</span>
+                </a>
+                <i class="bx bxs-chevron-down arrow"></i>
+            </div>
+            <ul class="sub-menu">
+                <li class="link-name"><a href="#">Tutor</a></li>
+                <li><a href="lectureReg.html">Add Tutor</a></li>
+                <li><a href="lectureUpDlt.html">Update/ Delete Tutor</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="icon-link">
+                <a href="">
+                    <i class="bx bx-file-blank"></i>
+                    <span class="link_name">Files</span>
+                </a>
+                <i class="bx bxs-chevron-down arrow"></i>
+            </div>
+            <ul class="sub-menu">
+                <li class="link-name"><a href="#">Files</a></li>
+                <li><a href="addFiles.html">Upload Files</a></li>
+                <li><a href="upDltFiles.html">Update/Delete Files </a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="icon-link">
+                <a href="">
+                    <i class="bx bxs-user"></i>
+                    <span class="link_name">Staff</span>
+                </a>
+                <i class="bx bxs-chevron-down arrow"></i>
+            </div>
+            <ul class="sub-menu">
+                <li class="link-name"><a href="#">Staff Regitration</a></li>
+                <li><a href="staffReg.html">Staff Regitration</a></li>
+                <li><a href="staffUpDlt.html">Update/Delete Staff</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="icon-link">
+                <a href="attendance.html">
+                    <i class="bx bx-calendar-check"></i>
+                    <span class="link_name">Attendance</span>
+                </a>
+            </div>
+            <ul class="sub-menu blank">
+                <li class="link-name "><a href="attendance.html">Attendance</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="icon-link">
+                <a href="Payment.html">
+                    <i class="bx bx-money"></i>
+                    <span class="link_name">Payment</span>
+                </a>
+            </div>
+            <ul class="sub-menu blank">
+                <li><a href="Payment.html" class="link_name">Payment</a></li>
+            </ul>
+        </li>
 
     </ul>
-  </div>
-  <header class="header">
+</div>
+<header class="header">
     <div class="toggle">
-      <i class='bx bx-menu'></i>
+        <i class='bx bx-menu'></i>
     </div>
 
     <nav class="navbar">
-      <ul>
-        <li><a href=""><img src="../img/user2.jpg" alt=""></a></li>
-        <li><a href=""><i class="bx bx-bell"></i></a></li>
-        <li><a href="#"><i class='bx bx-log-out-circle'></i></a></li>
+        <ul>
+            <li><a href=""><img src="../img/user2.jpg" alt=""></a></li>
+            <li><a href=""><i class="bx bx-bell"></i></a></li>
+            <li><a href="#"><i class='bx bx-log-out-circle'></i></a></li>
 
-      </ul>
-      </div>
+        </ul>
+        </div>
     </nav>
-  </header>
-  <!-- Student attendance section starts -->
-  <section class="attendance">
+</header>
+<!-- Student attendance section starts -->
+<section class="attendance">
     <h1>Student Attendance</h1>
 
-    <hr />
+    <hr/>
     <div class="rfidTrack">
         <form action="" method="post" enctype="multipart/form-data" name="addCourseF" class="addCourseF">
 
-      <label for="rfid">RFID Number:</label>
-      <input type="text" id="rfid" name="rfid" value="<?php echo $rfidno; ?>"/>
-       <input class="button button2" type="submit" id="addCrse" value="Verify" name="submit">
+            <label for="rfid">RFID Number:</label>
+            <input type="text" id="rfid" name="rfid" value="<?php echo $rfidno; ?>"/>
+            <input class="button button2" type="submit" id="addCrse" value="Verify" name="submit">
 
         </form>
     </div>
-    <hr />
+    <hr/>
     <div class="attendanceProcess">
-      <div class="userdetails">
-        <img src="img\avator.png" alt="userPicture" style="width: 200px; height: auto" />
-        <br />
-        <label for="sID">Student ID:</label>
-        <input type="text" id="rfidd" name="rfidd" value="<?php echo $StudentID; ?>"/>
-        <label for="studentName">Student Name:</label>
-          <input type="text" id="sname" name="sname" value="<?php echo $StudentName; ?>"/>
+        <div class="userdetails">
+            <img src="img\avator.png" alt="userPicture" style="width: 200px; height: auto"/>
+            <br/>
+            <label for="sID">Student ID:</label>
+            <input type="text" id="rfidd" name="rfidd" value="<?php echo $StudentID; ?>"/>
+            <label for="studentName">Student Name:</label>
+            <input type="text" id="sname" name="sname" value="<?php echo $StudentName; ?>"/>
 
-      </div>
-      <div class="payment-attendP">
-        <div class="paymentDetails">
-          <h1 class="paymentStatus">Payment Done</h1>
         </div>
-        <div class="attendPermission">
-          <a  class="button" href="attendance.php?delid=<?php echo ($rfidno); ?>" class="notiBtn" title="Delete" data-toggle="tooltip">Attend Allowed</a>
-          <a  class="button button3" href="attendance.php?delid=<?php echo ($rfidno); ?>" class="notiBtn" title="Delete" data-toggle="tooltip">Attend Deny</a>
+        <div class="payment-attendP">
+            <div class="paymentDetails">
+                <h1 class="paymentStatus">Payment Done</h1>
+            </div>
+            <div class="attendPermission">
+                <a class="button" href="attendance.php?delid=<?php echo($rfidno); ?>" class="notiBtn" title="Delete"
+                   data-toggle="tooltip">Attend Allowed</a>
+                <a class="button button3" href="attendance.php?delid=<?php echo($rfidno); ?>" class="notiBtn"
+                   title="Delete" data-toggle="tooltip">Attend Deny</a>
+            </div>
         </div>
-      </div>
     </div>
-  </section>
+</section>
 
-  <!-- Student attendace  section ends -->
-  <!-- custom js file -->
-  <script src="js/userP.js"></script>
+<!-- Student attendace  section ends -->
+<!-- custom js file -->
+<script src="js/userP.js"></script>
 </body>
 
 </html>
